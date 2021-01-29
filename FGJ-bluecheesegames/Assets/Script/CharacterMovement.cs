d1db1052c68251b423271aaf29651ce4ccd7aee1
balor001 <balor001@edu.xamk.fi>
2021-01-29 20:31:53 +0200

    Character movement
    - Added Character movement
    - Placeholder player and level sprites

---
+++ FGJ-bluecheesegames/Assets/Script/CharacterMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController2D characterController;
+    
+    float horizontalInput = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    public float runSpeed = 40f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Moves player
+        characterController.Move(horizontalInput * runSpeed * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+    }
+}
