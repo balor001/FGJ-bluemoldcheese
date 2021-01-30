a0f54dd673e5f15ba7c0eb21e98433f725bad104
bjola012 <55224564+bjola012@users.noreply.github.com>
2021-01-30 10:33:49 +0200

    Idle and walk animations

---
+++ FGJ-bluecheesegames/Assets/Material/Sprites/Teddy_Animations/AnimationTest.cs
@@ -0,0 +1,63 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+
+    private Animator playerAnim;
+    private int state;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        playerAnim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            state = 1;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            state = 2;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            state = 3;
+        }
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        switch (state)
+        {
+            case 1:
+                playerAnim.Play("Player_Walk", 0);
+                break;
+            case 2:
+                playerAnim.Play("Player_Swing", 0);
+                break;
+            case 3:
+                playerAnim.Play("Player_Idle", 0);
+                break;
+            default:
+                playerAnim.Play("Player_Idle", 0); 
+                break;
+        }
+    }
+}
+
